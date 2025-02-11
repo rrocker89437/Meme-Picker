0abe11f60589e895820da0d5041b1fd2871f9e19
rrocker89437 <ramiah.rocker@yahoo.com>
2025-02-10 22:22:02 -0800

    Updated css, html, and starting on js

---
+++ app.js
@@ -0,0 +1,26 @@
+import { catsData } from "./data.js";
+
+const emotionRadios = document.getElementById("emotion-radios");
+
+// Get Cat Emotions
+function getEmotionsArray(cats) {
+  const emotionsArray = [];
+  for (let cat of cats) {
+    for (let emotion of cat.emotionTags) {
+      emotionsArray.push(emotion);
+    }
+  }
+  return emotionsArray;
+}
+
+// Render Emotions Radios
+function renderEmotionsRadios(cats) {
+  let radioItems = ``;
+  const emotions = getEmotionsArray(cats);
+  for (let emotion of emotions) {
+    radioItems += `<p>${emotion}</p>`;
+  }
+  emotionRadios.innerHTML = radioItems;
+}
+
+renderEmotionsRadios(catsData);
